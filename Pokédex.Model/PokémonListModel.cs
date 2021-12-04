d2c16474a0cd2393ef377c28a2cb153ffa425ad6
Szabolcs Kiss <k.szabolcs623@gmail.com>
2021-12-04 20:24:05 +0100

    Mudblazor added to index, connected to json data

--- Pokédex.Model/PokémonListModel.cs
+++ Pokédex.Model/PokémonListModel.cs
@@ -8,16 +8,16 @@ namespace Pokédex.Model
 {
     public class Result
     {
-        public string name { get; set; }
-        public string url { get; set; }
+        public string? name { get; set; }
+        public string? url { get; set; }
     }
 
     public class PokémonList
     {
         public int count { get; set; }
-        public object next { get; set; }
-        public object previous { get; set; }
-        public List<Result> results { get; set; }
+        public object? next { get; set; }
+        public object? previous { get; set; }
+        public List<Result>? results { get; set; }
     }
 
 }
--- Pokédex/Pages/Index.razor.cs
+++ Pokédex/Pages/Index.razor.cs
@@ -7,14 +7,18 @@ namespace Pokédex.Pages
     public partial class Index
     {
         [Inject]
-        public HttpClient HttpClient { get; set; }
+        public HttpClient? HttpClient { get; set; }
 
-        public PokémonList PokemonList { get; set; }
+        public PokémonList? PokemonList { get; set; }
 
 
         protected async override Task OnInitializedAsync()
         {
+            if (HttpClient != null)
+            {
             PokemonList = await HttpClient.GetFromJsonAsync<PokémonList>("https://pokeapi.co/api/v2/pokemon?limit=1200");
+
+            }
         }
     }
 }

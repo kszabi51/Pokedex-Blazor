8a86b1a9927df32b0476d7f9b1fdeaa397650801
Szabolcs Kiss <k.szabolcs623@gmail.com>
2021-12-04 12:35:58 +0100

    Model project added with Pokémon List model

---
+++ Pokédex.Model/PokémonListModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokédex.Model
+{
+    public class PokémonListModel
+    {
+        public class Result
+        {
+            public string name { get; set; }
+            public string url { get; set; }
+        }
+
+        public class PokémonList
+        {
+            public int count { get; set; }
+            public object next { get; set; }
+            public object previous { get; set; }
+            public List<Result> results { get; set; }
+        }
+    }
+}

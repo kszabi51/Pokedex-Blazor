1e0b94b90a9df31a020e6ceb9d63f2d17b4918fe
Szabolcs Kiss <k.szabolcs623@gmail.com>
2021-12-11 12:04:13 +0100

    Pokemon names start with upper case

--- Pokédex.Model/PokémonListModel.cs
+++ Pokédex.Model/PokémonListModel.cs
@@ -11,14 +11,39 @@ namespace Pokédex.Model
         //Name from API
         public string? name { get; set; }
 
+        public string? RealName => GetPokemonName();
+
         //Url from API
         public string? url { get; set; }
 
         //Pokédex number split from the url
-        public int Number => int.Parse(url.Split('/').ElementAt(6));
+        public int Number
+        {
+            get
+            {
+                if (url != null)
+                {
+                    return int.Parse(url.Split('/').ElementAt(6));
+                }
 
-        //Picture from guthub repo using pokédex number
+                //To avoid crashes, return first pokemon
+                return 1;
+            }
+        }
+
+        //Picture from github repo using pokédex number
         public string? Picture => $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + Number.ToString() + ".png";
+
+        //Transfor pokemon names into user-readable format
+        private string? GetPokemonName()
+        {
+            string? returnName = string.Empty;
+            if (name != null)
+            {
+                returnName = string.Concat(name[0].ToString().ToUpper(), name.AsSpan(1));
+            }
+            return returnName;
+        }
 
     }
 

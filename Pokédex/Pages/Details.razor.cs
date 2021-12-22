d9dc9efd758ad40db50218daa1a51880fc544a1d
Szabolcs Kiss <k.szabolcs623@gmail.com>
2021-12-22 11:24:49 +0100

    Details HTTP logic added

--- Pokédex/Pages/Details.razor.cs
+++ Pokédex/Pages/Details.razor.cs
@@ -1,7 +1,23 @@
-﻿namespace Pokédex.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Pokédex.Model;
+using System.Net.Http.Json;
+
+namespace Pokédex.Pages
 {
     public partial class Details
     {
+        [Inject]
+        public HttpClient? HttpClient { get; set; }
+
+        public PokemonDetails? PokemonDetails { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (HttpClient != null)
+            {
+                PokemonDetails = await HttpClient.GetFromJsonAsync<PokemonDetails>("https://pokeapi.co/api/v2/pokemon/3");
+            }
+        }
 
     }
 }

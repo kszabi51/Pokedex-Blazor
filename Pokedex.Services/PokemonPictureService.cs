a24a4973785bb89c612c9788cf5bdc06fb6bcc5e
Szabolcs Kiss <k.szabolcs623@gmail.com>
2022-11-17 18:53:35 +0100

    Simplified service method

--- Pokedex.Services/PokemonPictureService.cs
+++ Pokedex.Services/PokemonPictureService.cs
@@ -5,9 +5,6 @@
         private static readonly string baseUrl = $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
         private static readonly string fileExtension = ".png";
 
-        public static string? GetPictureUrl(string? numberString)
-        {
-            return string.Concat(baseUrl, numberString, fileExtension);
-        }
+        public static string? GetPictureUrl(string? pokemonId) => string.Concat(baseUrl, pokemonId, fileExtension);
     }
 }

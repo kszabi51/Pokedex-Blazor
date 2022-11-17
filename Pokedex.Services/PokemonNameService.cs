a266806ed19a35174c5067f69872a9554ce906db
Szabolcs Kiss <k.szabolcs623@gmail.com>
2022-11-17 18:49:50 +0100

    Removed usings from Services

--- Pokedex.Services/PokemonNameService.cs
+++ Pokedex.Services/PokemonNameService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pokedex.Services
+﻿namespace Pokedex.Services
 {
     public static class PokemonNameService
     {

cc5fa1fbf35a883acd3c6fa92bbb23f1befa5df3
Szabolcs Kiss <k.szabolcs623@gmail.com>
2024-03-12 13:37:39 +0100

    PictureService tests added

---
+++ Pokedex.Services.Tests/PokemonPictureServiceTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Pokedex.Tests
+{
+    public class PokemonPictureServiceTests
+    {
+        [Test]
+        public void GetPictureUrl_ValidId_ReturnsCorrectUrl()
+        {
+            // Arrange
+            var pokemonId = "25";
+
+            // Act
+            var result = PokemonPictureService.GetPictureUrl(pokemonId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"));
+        }
+
+        [Test]
+        public void GetPictureUrl_NullId_ReturnsBaseUrl()
+        {
+            // Arrange
+            string? pokemonId = null;
+
+            // Act
+            var result = PokemonPictureService.GetPictureUrl(pokemonId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/.png"));
+        }
+    }
+}

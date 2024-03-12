580d6e4862289768aaf5b597334f2eb1dec5e771
Szabolcs Kiss <k.szabolcs623@gmail.com>
2024-03-12 12:38:05 +0100

    New test added, NumberService refactor

---
+++ Pokedex.Services.Tests/PokemonDetailsServices/PokemonWeightServiceTests.cs
@@ -0,0 +1,16 @@
+using Pokedex.Services;
+
+namespace Pokedex.Tests
+{
+    [TestFixture]
+    public class PokemonWeightServiceTests
+    {
+        [TestCase(10, ExpectedResult = "1 kg")]
+        [TestCase(20, ExpectedResult = "2 kg")]
+        [TestCase(0, ExpectedResult = "0 kg")]
+        public string? GetPokemonWeight_ReturnsCorrectWeight(int weight)
+        {
+            return PokemonWeightService.GetPokemonWeight(weight);
+        }
+    }
+}
--- Pokedex.Services/PokemonNumberService.cs
+++ Pokedex.Services/PokemonNumberService.cs
@@ -2,16 +2,26 @@
 {
     public static class PokemonNumberService
     {
+        private const int DefaultPokemonNumber = 1;
+        private const int PokemonNumberPositionInUrl = 6;
+
         public static int GetPokemonNumber(string? url)
         {
             //Split the number from the url
             if (url != null)
             {
-                return int.Parse(url.Split('/').ElementAt(6));
+                var urlSegments = url.Split('/');
+                if (urlSegments.Length > PokemonNumberPositionInUrl)
+                {
+                    if (int.TryParse(urlSegments[PokemonNumberPositionInUrl], out int pokemonNumber))
+                    {
+                        return pokemonNumber;
+                    }
+                }
             }
 
-            //To avoid crashes, return first pokemon
-            return 1;
+            // To avoid crashes, return first pokemon
+            return DefaultPokemonNumber;
         }
     }
 }

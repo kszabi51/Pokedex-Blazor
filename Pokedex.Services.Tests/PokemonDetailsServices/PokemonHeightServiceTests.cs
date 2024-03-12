180a49848bff38ab1d3e290b782fcf8d9c19934e
Szabolcs Kiss <k.szabolcs623@gmail.com>
2024-03-12 12:27:00 +0100

    Unit Test project created

---
+++ Pokedex.Services.Tests/PokemonDetailsServices/PokemonHeightServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Pokedex.Services;
+
+namespace Pokedex.Tests
+{
+    [TestFixture]
+    public class PokemonHeightServiceTests
+    {
+        [TestCase(10, ExpectedResult = "1 m")]
+        [TestCase(20, ExpectedResult = "2 m")]
+        [TestCase(0, ExpectedResult = "0 m")]
+        public string? GetPokemonHeight_ReturnsCorrectHeight(int height)
+        {
+            return PokemonHeightService.GetPokemonHeight(height);
+        }
+    }
+}
